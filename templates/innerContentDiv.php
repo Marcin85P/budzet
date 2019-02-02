d7dc3d39c0e747a7a6111ffcbfc71a17d0a6ddf8
Marcin <marcin.piwowar.programista@gmail.com>
2019-02-02 19:39:08 +0100

    corect

--- templates/innerContentDiv.php
+++ templates/innerContentDiv.php
@@ -6,46 +6,22 @@
 	}
 ?>
 
-		<link rel="stylesheet" href="main.css" type="text/css" />
-		<header>
-			<nav class="page-navigation clearfix">
-			
-				<label class="navigation-toggle" for="input-toggle">
-					<span></span>
-					<span></span>
-					<span></span>
-				</label>
-			
-				<input type="checkbox" id="input-toggle">
-				
-				<ul class="menu">
-					<li><a href='index.php?action=showIncomes'>Dodaj przychód</a></li>
-					<li><a href='index.php?action=showExpenses'>Dodaj wydatek</a></li>
-					<li><a href='index.php?action=tableView'>Przegląd bilansu</a></li>
-					<li><a href='index.php?action=settingsView'>Ustawienia</a></li>
-					<li><a href='index.php?action=logout' class="logout">Wyloguj się</a></li>	
-				</ul>
-					
-			</nav>
-		</header>
-	
-		<main>
-				<div class="con">
-				
-					<div class="article">
-
-					<p>
-					<?php
-						if(!isset($komunikat))
-							echo 'Strona, która pomaga kontrolować Twoje wydatki. Przeglądaj swój bilans i sprawdź czy w ostatnim czasie zaoszczędziłeś czy się zadłużyłeś.';
-						else{
-							echo $komunikat;
-							echo "</br>";
-							echo "<a href='index.php?action=showMain'><input type='button' value='OK'></a>";
-						}?>
-					</p>
-					
-					</div>
-					
-				</div>
-		</main>
+<div class="container">
+	<div class="row">
+		<div class="col-12">
+			<div class="infowin" style="width: 100%;">
+				<h3>Budżet</h3>
+				<p>Nadzór nad budżetem to podstawowe działanie, które powinien podjąć każdy, aby rozsądnie rozporządzać zasobami finansowymi.
+				Dzięki prowadzeniu ewidencji dochodów i wydatków można dokładnie określić miesięczne koszty związane ze stylem i jakością życia.
+				Wiedza, dotycząca rozchodu zarobionych pieniędzy, daje poczucie stabilizacji i bezpieczeństwa.</br>
+				Człowiek wzbogaca się w momencie, kiedy wydaje mniej pieniędzy niż zarabia. Warto o tym pamiętać, ponieważ jest to złota zasada ludzi osiągających sukces finansowy.</p>
+			</div>
+		</div>
+		
+		<div class="col-12">
+			<div class="infowin" style="width: 100%; text-align: center; padding: 0; ">
+				<img style="opacity:0.7;" src="img/piggy-bank-with-coin.png" width="130"></img>
+			</div>
+		</div>	
+	</div>	
+</div>
